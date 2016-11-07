58feeece31b9a055221378a58453d8df22bb1e72
Danil Chekalin <dakiesse@gmail.com>
2016-11-07 19:53:26 +0200

    Refactoring

---
+++ src/fio-presenter.js
@@ -0,0 +1,39 @@
+function capitalize([first, ...rest]) {
+  return first.toUpperCase() + rest.join('').toLowerCase();
+}
+
+/**
+* @param {string} fullname
+*/
+export default function fioPresenter(fullname) {
+  let result,
+    nameParts,
+    firstname,
+    middlename,
+    lastname;
+
+  const regexp = /^(?!.*(_|\/).*)[\D\s]+$/;
+
+  if (!regexp.test(fullname)) {
+    nameParts = [];
+  } else {
+    nameParts = fullname.split(' ');
+  }
+
+  nameParts = nameParts.map(capitalize);
+
+  switch (nameParts.length) {
+    case 3:
+      [firstname, middlename, lastname] = nameParts;
+      result = `${lastname} ${firstname[0]}. ${middlename[0]}.`; break;
+    case 2:
+      [firstname, lastname] = nameParts;
+      result = `${lastname} ${firstname[0]}.`; break;
+    case 1:
+      result = capitalize(fullname); break;
+    default:
+      result = 'Invalid fullname';
+  }
+
+  return result;
+}
--- src/index.js
+++ src/index.js
@@ -1,60 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import fioPresenter from './fio-presenter';
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({hello: 'JS World'});
-});
+
+app.get('/', (req, res) => res.json({ hello: 'JS World' }));
 
 app.get('/task2A', (req, res) => {
-  const sum = (+ req.query.a || 0) + (+ req.query.b || 0);
+  const sum = (Number(req.query.a) || 0) + (Number(req.query.b) || 0);
 
-  res.send(sum.toString());
+  return res.send(sum.toString());
 });
 
-const capitalize = ([first, ...rest]) => first.toUpperCase() + rest.join('').toLowerCase();
-
 app.get('/task2B', (req, res) => {
-  let result,
-    nameParts,
-    firstname,
-    middlename,
-    lastname;
-
   const fullname = req.query.fullname
-  .trim()
-  .toLowerCase()
-  .replace(/\s\s+/g, ' '); // Remove multiple whitespaces
+    .trim()
+    .toLowerCase()
+    .replace(/\s\s+/g, ' '); // Remove multiple whitespaces
 
-  const regexp = /^(?!.*(_|\/).*)[\D\s]+$/;
+  const presentedFullName = fioPresenter(fullname);
 
-  if (!regexp.test(fullname)) {
-    nameParts = [];
-  } else {
-    nameParts = fullname.split(' ');
-  }
-
-  // Capitalize all items
-  nameParts = nameParts.map(capitalize);
-
-  switch (nameParts.length) {
-    case 3:
-      [firstname, middlename, lastname] = nameParts;
-      result = `${lastname} ${firstname[0]}. ${middlename[0]}.`;
-      break;
-    case 2:
-      [firstname, lastname] = nameParts;
-      result = `${lastname} ${firstname[0]}.`;
-      break;
-    case 1:
-      result = req.query.fullname;
-      break;
-    default:
-      result = 'Invalid fullname';
-  }
-
-  res.send(result);
+  return res.send(presentedFullName);
 });
 
 app.listen(3000);
