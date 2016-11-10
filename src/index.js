713091a04210e13c3fa1bb0cb5cd733802fb7932
Danil Chekalin <dakiesse@gmail.com>
2016-11-10 21:20:02 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -26,19 +26,15 @@ app.get('/task2B', (req, res) => {
 });
 
 app.get('/task2C', (req, res) => {
-  const rawUsernameParts = req.query.username.trim().split('//');
+  const rawUsernameParts = req.query.username.trim().split('//'); // remove any protocols
   const re = /^([A-Z\.:+\d-]+\/)?@?([\w.-_]+)((\?|\/).*)?$/i;
 
   let result;
 
   switch (rawUsernameParts.length) {
-    case 2:
-      rawUsernameParts[0] = rawUsernameParts[1]; // non-break
-    case 1:
-      result = rawUsernameParts[0].match(re)[2];
-      break;
-    default:
-      result = 'Invalid username';
+    case 2: rawUsernameParts[0] = rawUsernameParts[1]; // non-break
+    case 1: result = rawUsernameParts[0].match(re)[2]; break;
+    default: result = 'Invalid username';
   }
 
   return res.send(`@${result}`);
