7f2158d76c206b5a18bc7c81aca5649346944d66
Danil Chekalin <dakiesse@gmail.com>
2016-11-22 01:25:57 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -60,26 +60,25 @@ app.get('/task2D', (req, res) => {
   const matchRe = /([\da-f])([\da-f])([\da-f])/i;
 
   try {
-    let rawColorHex = req.query.color.trim().toLowerCase()
+    let rawAnyColor = req.query.color.trim().toLowerCase()
       .replace(/%23/g, '#')
       .replace(/%20/g, '')
       .replace(/\s/g, '');
 
-    if (hexRe.test(rawColorHex)) {
-      if (rawColorHex[0] === '#') rawColorHex = rawColorHex.substr(1);
+    if (hexRe.test(rawAnyColor)) {
+      if (rawAnyColor[0] !== '#') rawAnyColor = `#${rawAnyColor}`;
 
-      result = colorcolor(`#${rawColorHex}`, 'hex');
-    } else if (rgbaRe.test(rawColorHex) && checkRgb(rawColorHex)) {
-      result = colorcolor(rawColorHex, 'hex');
-    } else if (hslRe.test(rawColorHex) && chechHsl(rawColorHex)) {
-      result = colorcolor(rawColorHex, 'hex');
+      result = colorcolor(rawAnyColor, 'hex');
+    } else if (rgbaRe.test(rawAnyColor) && checkRgb(rawAnyColor)) {
+      result = colorcolor(rawAnyColor, 'hex');
+    } else if (hslRe.test(rawAnyColor) && chechHsl(rawAnyColor)) {
+      result = colorcolor(rawAnyColor, 'hex');
     } else {
       throw new Error();
     }
 
     return res.send(result);
   } catch (e) {
-    console.log(e);
     return res.send('Invalid color');
   }
 });
